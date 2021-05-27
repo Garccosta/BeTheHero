c9ef00ec2eeaaebc6e315075154d8f130eebae4e
Garccosta <garccosta@gmail.com>
2021-05-27 09:46:59 -0300

    Added basic backend validation

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,3 +1,4 @@
+const { errors } = require('celebrate');
 const connection = require('../database/connection');
 
 module.exports={
@@ -51,7 +52,8 @@ module.exports={
         if (incident.ong_id !== ong_id){
             return response.status(401).json({error: 'Operation not permitted.'});
         }
-        await connection('incidents').where('id', id).delete();
+        
+        await connection('incidents').where('id', id).delete()
 
         return response.status(204).send();
     }
