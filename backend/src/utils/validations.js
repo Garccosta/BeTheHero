e67f6a935128e5f9413a04abf28db0b69b5c8bb4
Garccosta <garccosta@gmail.com>
2021-06-05 18:03:04 -0300

    Added eslint and lint fixes

--- backend/src/utils/validations.js
+++ backend/src/utils/validations.js
@@ -3,37 +3,39 @@ const { celebrate, Segments, Joi } = require('celebrate');
 
 const validations =
 {
-  ongs: 
-    celebrate({
-      [Segments.BODY]: Joi.object().keys({
-          name: Joi.string().required(),
-          email: Joi.string().required().email(),
-          whatsapp: Joi.string().required().min(10).max(11),
-          city: Joi.string().required(),
-          uf: Joi.string().required().length(2),
-      })  
-    }),
-  profile:
-    celebrate({
-      [Segments.HEADERS]: Joi.object({
-          authorization: Joi.string().required()
-      }).unknown()
-    }), 
-  incidents:
-    {
-      get:
-        celebrate({
-          [Segments.QUERY]: Joi.object().keys({
-              page: Joi.number()
-          })
-        }), 
-      delete:
-        celebrate({
-          [Segments.PARAMS]: Joi.object().keys({
-              id: Joi.number().required()
-          })
-        })
-    }
-}
+	ongs: 
+		celebrate({
+			[Segments.BODY]: Joi.object().keys({
+				name: Joi.string().required(),
+				email: Joi.string().required().email(),
+				whatsapp: Joi.string().required().min(10).max(11),
+				city: Joi.string().required(),
+				uf: Joi.string().required().length(2),
+			})  
+		}),
+		
+	profile:
+		celebrate({
+			[Segments.HEADERS]: Joi.object({
+				authorization: Joi.string().required()
+			}).unknown()
+		}), 
+
+	incidents:
+		{
+			get:
+				celebrate({
+					[Segments.QUERY]: Joi.object().keys({
+						page: Joi.number()
+					})
+				}), 
+			delete:
+				celebrate({
+					[Segments.PARAMS]: Joi.object().keys({
+						id: Joi.number().required()
+					})
+				})
+		}
+};
 
 module.exports = validations;
--- backend/tests/integration/ong.test.js
+++ backend/tests/integration/ong.test.js
@@ -1,26 +1,27 @@
+/* eslint-disable no-undef */
 const request = require('supertest');
 const app = require('../../src/app');
 const connection = require('../../src/database/connection');
 const faker = require('../Factories/FakerFactory');
 
 describe('ONG', () => {
-  beforeAll(async () => {
-    await connection.migrate.latest();
-  });
+	beforeAll(async () => {
+		await connection.migrate.latest();
+	});
 
-  afterAll(async () => {
-    await connection('ongs').truncate();
-    await connection.destroy();
-  });
+	afterAll(async () => {
+		await connection('ongs').truncate();
+		await connection.destroy();
+	});
 
-  it('should create a new ONG', async () => {
-    let ONG = faker.CreateFakeONG();
+	it('should create a new ONG', async () => {
+		let ONG = faker.CreateFakeONG();
     
-    const response = await request(app)
-    .post('/ongs')
-    .send(ONG)
+		const response = await request(app)
+			.post('/ongs')
+			.send(ONG);
 
-    expect(response.body).toHaveProperty('id');
-    expect(response.body.id).toHaveLength(8);
-  });
-})
+		expect(response.body).toHaveProperty('id');
+		expect(response.body.id).toHaveLength(8);
+	});
+});
